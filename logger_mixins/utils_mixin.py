62cd915459edaf2df645c0ad333b53d6163cae47
lummetry-team <tech@lummetry.ai>
2022-03-07 09:23:37 +0000

    deployment

--- logger_mixins/utils_mixin.py
+++ logger_mixins/utils_mixin.py
@@ -85,12 +85,12 @@ class _UtilsMixin(object):
     # self-referential objects
     seen.add(obj_id)
     if isinstance(obj, dict):
-      size += sum([_NoBucketFunctionalitiesMixin.get_obj_size(v, seen) for v in obj.values()])
-      size += sum([_NoBucketFunctionalitiesMixin.get_obj_size(k, seen) for k in obj.keys()])
+      size += sum([_UtilsMixin.get_obj_size(v, seen) for v in obj.values()])
+      size += sum([_UtilsMixin.get_obj_size(k, seen) for k in obj.keys()])
     elif hasattr(obj, '__dict__'):
-      size += _NoBucketFunctionalitiesMixin.get_obj_size(obj.__dict__, seen)
+      size += _UtilsMixin.get_obj_size(obj.__dict__, seen)
     elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
-      size += sum([_NoBucketFunctionalitiesMixin.get_obj_size(i, seen) for i in obj])
+      size += sum([_UtilsMixin.get_obj_size(i, seen) for i in obj])
 
     return size
 
