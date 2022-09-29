e68a21c1280542a74117f9c002252a5674fa65fe
lummetry-team <tech@lummetry.ai>
2022-09-29 07:07:07 +0000

    deployment

--- plugins_manager_mixin.py
+++ plugins_manager_mixin.py
@@ -57,27 +57,32 @@ class _PluginsManagerMixin:
   
   def _perform_module_safety_check(self, module):
     good = True
+    msg = ''
     str_code = inspect.getsource(module)
-    self.P("Performing code safety check on module {}:".format(module.__name__), color='m')
+    self.P("  Performing code safety check on module {}:".format(module.__name__), color='m')
     ### TODO: finish code analysis using BaseCodeChecker
     errors = self.code_checker.check_code_text(str_code)
     if errors is not None:      
-      self.P("  ERROR: Unsafe code in {}: {}.".format(
-        module.__name__, errors), color='error'
-      )
+      msg = "  ERROR: Unsafe code in {}:\n{}".format(
+        module.__name__, '\n'.join(
+          ['  *** ' +msg + 'at line(s): {}'.format(v) for msg, v in errors.items()]
+          ))
+      self.P(msg, color='error')
       self.P("   ********* In future this will STOP the usage of this plugin. **************", color='r')
-      # now return ... bad
-    self.P("Finished performing code safety check on module {}:".format(module.__name__), color='m')
-    return good
+      # now set good = False
+    # endif bad code
+    self.P("  Finished performing code safety check on module {}:".format(module.__name__), color='m')
+    return good, msg
 
   def _perform_class_safety_check(self, classdef):
     good = True
+    msg = ''
     str_code = inspect.getsource(classdef)
     self.P("Performing code safety check on class {}:".format(classdef.__name__), color='m')
     ### TODO: finish code analysis using BaseCodeChecker
-    return good
+    return good, msg
 
-  def _get_module_name_and_class(self, locations, name, suffix=None, verbose=1, safety_check=False):
+  def _get_module_name_and_class(self, locations, name, suffix=None, verbose=1, safety_check=False, safe_locations=None):
     if not isinstance(locations, list):
       locations = [locations]
 
@@ -88,18 +93,38 @@ class _PluginsManagerMixin:
       suffix = ''
 
     suffix = suffix.replace('_', '')
-
-    _module_name = self._get_plugin_by_name(locations, simple_name)
+    
+    _safe_module_name = None
+    is_safe_plugin = False
+    # first search is safe locations always!
+    if safe_locations is not None and isinstance(safe_locations, list) and len(safe_locations) > 0:
+      _safe_module_name = self._get_plugin_by_name(safe_locations, simple_name)
+    if _safe_module_name is None:
+      _user_module_name = self._get_plugin_by_name(locations, simple_name)
+      _module_name = _user_module_name
+    else:
+      is_safe_plugin = True 
+      _module_name = _safe_module_name
+    
+    
     if _module_name is None:
       if verbose >= 1:
         self.P("Error with finding plugin '{}' in locations '{}'".format(simple_name, locations), color='r')
       return _module_name, _class_name, _cls_def, _config_dict
 
+    self.P("  Found {} plugin '{}'".format(
+      'safe' if is_safe_plugin else 'user', name), 
+      color='g' if is_safe_plugin else 'm'
+    )
+    
+    safety_check = False if is_safe_plugin else safety_check
+
     try:
       module = importlib.import_module(_module_name)
       if module is not None and safety_check:
-        if not self._perform_module_safety_check(module):
-          raise ValueError("Unsafe code detected in module '{}'".format(module.__name__))
+        is_good, msg = self._perform_module_safety_check(module)
+        if not is_good:
+          raise ValueError("Unsafe code exception: {}".format(msg))
       classes = inspect.getmembers(module, inspect.isclass)
       for _cls in classes:
         if _cls[0].upper() == simple_name.upper() + suffix.upper():
@@ -111,8 +136,9 @@ class _PluginsManagerMixin:
           ), color='r')
       _config_dict = getattr(module, "_CONFIG", None)
       if _cls_def is not None and safety_check:
-        if not self._perform_class_safety_check(_cls_def):
-          raise ValueError("Unsafe code detected in class '{}'".format(_cls_def.__name__))     
+        is_good, msg = self._perform_class_safety_check(_cls_def)
+        if not is_good:
+          raise ValueError("Unsafe class code exception: {}".format(msg))     
       _found_location = ".".join(_module_name.split('.')[:-1])
       self.P("  Plugin '{}' loaded and checked from {}".format(name, _found_location), color='g')
     except:
