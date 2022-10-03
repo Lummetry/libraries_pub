9b0d0c3773c7deec783498c5f7039953fe9103e7
lummetry-team <tech@lummetry.ai>
2022-10-03 11:39:39 +0000

    deployment

---
+++ version.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2019-2021 Lummetry.AI (Knowledge Investment Group SRL). All Rights Reserved.
+
+
+* NOTICE:  All information contained herein is, and remains
+* the property of Knowledge Investment Group SRL.  
+* The intellectual and technical concepts contained
+* herein are proprietary to Knowledge Investment Group SRL
+* and may be covered by Romanian and Foreign Patents,
+* patents in process, and are protected by trade secret or copyright law.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Knowledge Investment Group SRL.
+
+
+@copyright: Lummetry.AI
+@author: Lummetry.AI
+@project: 
+@description:
+@created on: Sun Sep 25 20:45:26 2022
+@created by: AID
+"""
+
+
+__VER__ = '9.6.9'
